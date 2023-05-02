c62f0441a08ece9e6cc39dfe0227b0744ea85aae
UstDenisGb <122019774+UstDenisGb@users.noreply.github.com>
2023-05-02 23:46:30 +0300

    Add files via upload

---
+++ S1.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class S1 {
+    public static void main (String[] args) {
+        Random i = new Random();
+        System.out.println(i.nextInt(2001));
+    }
+
+    }
+
+
